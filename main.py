f59da1e4d490e222e65d0166238aa6da218dd0fa
Andrey Boris Khesin <andrey.khesin@gmail.com>
2024-07-30 17:02:30 -0400

    various codes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from _graph2code import find_distance
 from _code_dipyramid import adj_dipyramid
 from _code_platonic import *
 from _code_simple import adj_simple
-from _code_lattice import adj_lattice, adj_3lattice
+from _code_lattice import *
 from _code_simplex import adj_simplex
 #from _code_archimedes import adj_archimedes
 """
@@ -23,15 +23,34 @@ Basic usage:
 (4) Run and see what the distance is!
 """
 
-# ADJ_MAT = adj_covered_icosahedron(5)
-# INPUTS = [10+0, 6+12, 8+24, 2+24, 11+36, 5+48]
-# print(ADJ_MAT)
-
 #k = 6
 #ADJ_MAT = adj_simplex(k)
 #INPUTS = list(range(k))
 
-ADJ_MAT=adj_platonic('dodecahedron')
+#PARAMS [16, 4, 3]
+#ADJ_MAT=adj_platonic('dodecahedron')
+#visualize_platonic_solid(platonic_solid_graph('dodecahedron'))
+#INPUTS = [0, 6, 13, 17]
+
+#PARAMS [22, 2, 5]
+#ADJ_MAT = adj_covered_icosahedron(2)
+#visualize_platonic_solid(platonic_solid_graph('icosahedron'))
+#INPUTS = [0, 0 + 12]
+
+#PARAMS [54, 6, 5]
+#ADJ_MAT = adj_covered_icosahedron(5)
+#INPUTS = [10, 6 + 12, 8 + 24, 2 + 24, 11 + 36, 5 + 48]
+
+#PARAMS [48, 1, 6]
+#ADJ_MAT = adj_lattice(7, 7)
+#INPUTS = [0]
+
+#PARAMS [44, 1, 6]
+#ADJ_MAT = cubic_lattice(5, 3, 3)
+#INPUTS = [0]
+
+#POSSIBLE PARAMS [53, 1, 7]
+ADJ_MAT = adj_3lattice(3, 3, 3)
 INPUTS = [0]
 
 d = find_distance(ADJ_MAT, INPUTS)
